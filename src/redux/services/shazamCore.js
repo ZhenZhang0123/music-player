acda171f20437b6b06614ae3e5ef7a25489ebac1
zhenzhang0123 <zhen.zhang0123@gmail.com>
2024-03-04 19:18:22 -0500

    songcard img&&title

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const shazamCoreApi = createApi({
   reducerPath: "shazamCoreApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://shazam-core7.p.rapidapi.com",
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
     prepareHeaders: (headers) => {
       headers.set(
         "X-RapidAPI-Key",
@@ -15,7 +15,7 @@ export const shazamCoreApi = createApi({
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({
-      query: () => "/charts/get-top-songs-in-world",
+      query: () => "/charts/world",
     }),
   }),
 });
